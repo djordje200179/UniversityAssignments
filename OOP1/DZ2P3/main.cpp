232bd4d4865085fa94aee99a3f4ff10a94b77854
Đorđe Milanović <djordje200179@gmail.com>
2021-11-24 16:01:03 +0100

    Testing improved

--- OOP1/DZ2P3/main.cpp
+++ OOP1/DZ2P3/main.cpp
@@ -4,17 +4,8 @@
 
 using namespace std;
 
-int main() {
-	cout << std::boolalpha;
-
-	Skup skup("ana voli milovana");
-
-	cout << "n: " << skup('n') << endl;
-	cout << "g: " << skup('g') << endl;
-	skup += 'g';
-	cout << "g: " << skup('g') << endl;
-
-	Rijec rijec("medvedic");
+void ispisiInfo(const Rijec& rijec) {
+	cout << endl;
 	cout << rijec << '=' << +rijec << '/' << ~rijec << endl;
 	cout << "slog[0] = " << rijec(0) << endl;
 	cout << "slog[1] = " << rijec(1) << endl;
@@ -23,9 +14,32 @@ int main() {
 	cout << "slog[-1] = " << rijec(-1) << endl;
 	cout << "slog[-2] = " << rijec(-2) << endl;
 	cout << "slog[-3] = " << rijec(-3) << endl;
+	cout << endl;
+}
+
+int main() {
+	cout << std::boolalpha;
+
+	Skup skup("ana voli milovana");
+
+	cout << "skup[n]: " << skup('n') << endl;
+	cout << "skup[g]: " << skup('g') << endl;
+	skup += 'g';
+	cout << "skup[g]: " << skup('g') << endl;
 
 	cout << "dugoprstic ^ prstic = " << (Rijec("dugoprstic") ^ Rijec("prstic")) << endl;
 	cout << "slonic ^ prstic = " << (Rijec("slonic") ^ Rijec("prstic")) << endl;
+
+	Rijec rijec("medvedic");
+	ispisiInfo(rijec);
+
+	cout << "Unesi rijec" << endl;
+	cin >> rijec;
+	ispisiInfo(rijec);
+
+	cout << "Unesi rijec" << endl;
+	cin >> rijec;
+	ispisiInfo(rijec);
 
 	return 0;
 }
