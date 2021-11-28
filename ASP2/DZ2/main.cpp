234781c8831fd1dca76e7aa43ddf28d76d166dba
Đorđe Milanović <djordje200179@gmail.com>
2021-11-28 21:54:14 +0100

    Menu added

--- ASP2/DZ2/main.cpp
+++ ASP2/DZ2/main.cpp
@@ -32,16 +32,89 @@ void testPointers(const BStarTree& tree) {
 }
 
 int main() {
-	BStarTree tree(4);
+	BStarTree* tree = nullptr;
+	cout << boolalpha;
 
-	ifstream file("sample.txt");
-	file >> tree;
+	while (true) {
+		cout << "Odaberite opciju: " << endl;
+		cout << "1) Kreiraj stablo" << endl;
+		cout << "2) Unisti stablo" << endl;
+		cout << "3) Inicijalizuj stablo" << endl;
+		cout << "4) Ispisi stablo" << endl;
+		cout << "5) Provjeri kljuc" << endl;
+		cout << "6) Umetni kljuc" << endl;
+		cout << "7) Obrisi kljuc" << endl;
+		cout << "8) Pronadji k-ti element" << endl;
+		cout << "9) Zavrsi program" << endl;
 
-	cout << tree << endl;
-	testPointers(tree);
+		int choice;
+		cin >> choice;
 
-	for(int i = 0; i < 15; i++)
-		cout << tree.findKthKey(i) << endl;
+		string tempS;
+		int tempI;
+		switch (choice) {
+		case 1:
+			cout << "Unesi stepen: ";
+			cin >> tempI;
+
+			delete tree;
+			tree = new BStarTree(tempI);
+
+			break;
+		case 2:
+			delete tree;
+			tree = nullptr;
+
+			break;
+		case 3:
+			cout << "Unesi ime fajla: ";
+			cin >> tempS;
+			
+			{
+				ifstream file(tempS, ios_base::in);
+				file >> *tree;
+				file.close();
+			}
+
+			break;
+		case 4:
+			cout << *tree;
+
+			break;
+		case 5:
+			cout << "Kljuc: ";
+			cin >> tempS;
+
+			cout << "Nalazi se: " << tree->keyExists(tempS) << endl;
+
+			break;
+		case 6:
+			cout << "Kljuc: ";
+			cin >> tempS;
+
+			cout << "Dodan: " << tree->addKey(tempS) << endl;
+
+			break;
+		case 7:
+			cout << "Kljuc: ";
+			cin >> tempS;
+
+			cout << "Izbrisan: " << tree->removeKey(tempS) << endl;
+
+			break;
+		case 8:
+			cout << "k: ";
+			cin >> tempI;
+
+			cout << "k-ti kljuc: " << tree->findKthKey(tempI) << endl;
+
+			break;
+		case 9:
+			delete tree;
+
+			return 0;
+		}
+	}
 
 	return 0;
 }
