830db1ae7c55272c9f4ab8a749c0c67573a52be1
Đorđe Milanović <djordje200179@gmail.com>
2021-12-22 17:30:43 +0100

    Function implementation fixed

--- ASP2/DZ3/AddressFunction.cpp
+++ ASP2/DZ3/AddressFunction.cpp
@@ -10,5 +10,7 @@ void AddressFunction::init(HashTable* hashTable) const {
 unsigned int SplitSequenceLinearHashing::getAddress(unsigned int key, int address, int attempt, int size) const {
 	auto toCompare = hashTable->table[address].front();
 
-	return (address + attempt * (true ? s1 : s2)) % size; // popraviti uslov
+	auto movement = toCompare->getId() > key ? s1 : s2;
+
+	return (address + attempt * movement) % size;
 }
