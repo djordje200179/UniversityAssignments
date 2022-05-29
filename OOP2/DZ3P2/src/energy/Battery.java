d1b46ef944e685ab03981f9c7a751817a280e347
Đorđe Milanović <Djord@DJORDJE-MILANOV>
2022-05-29 18:27:28 +0200

    Synchronization fixed

--- OOP2/DZ3P2/src/energy/Battery.java
+++ OOP2/DZ3P2/src/energy/Battery.java
@@ -1,15 +1,17 @@
 package energy;
 
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Battery {
-	private int currentEnergy;
+	private final AtomicInteger currentEnergy;
 	private final int capacity;
 
 	public Battery(int capacity) {
 		this.capacity = capacity;
-		this.currentEnergy = capacity;
+		this.currentEnergy = new AtomicInteger(capacity);
 	}
 
-	public void addEnergy(int energy) { currentEnergy = Math.max(currentEnergy + energy, capacity); }
-	public void empty() { currentEnergy = 0; }
-	public boolean isFull() { return currentEnergy == capacity; }
+	public void addEnergy(int energy) {	currentEnergy.updateAndGet(old -> Math.min(old + energy, capacity)); }
+	public void empty() { currentEnergy.set(0); }
+	public boolean isFull() { return currentEnergy.get() == capacity; }
 }
